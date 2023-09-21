ff8d52dbf5b637f354894a867615b3440c3b58c0
Jon O'Connell <jjoc0205@gmail.com>
2023-09-21 08:33:06 -0400

    trying to use resusable components

---
+++ js/header.js
@@ -0,0 +1,38 @@
+class Header extends HTMLElement{
+    constructor(){
+        super();
+    }
+    connectedCallback(){
+        this.innerHTML = `
+        <style>
+            .header{
+                background-color: rgb(250, 249, 250);
+                text-align: end;
+                overflow: auto;
+                height: 60px;
+            }
+            .links{
+                color: #5649e9;
+                font-size: 30px;
+                text-decoration: none;
+                padding-top: 25px;
+                margin-left: 25px;
+                margin-right: 15px;
+            }
+            .links:hover{
+                color: #e95454;
+            }
+        </style>
+        <header>
+        <div class="header">
+            <a href="https://jfmarks.github.io/" class="links">Previous Student</a>
+            <a href="index.html" class="links">Home</a>
+            <a href="/aboutMe.html" class="links">About me page</a>
+            <a href="https://mattm401.github.io/" class="links">Course Site</a>
+            <a href="https://jthensley03.github.io/" class="links">Next Student</a>
+        </div>
+        </header>
+    `;
+    }
+}
+customElements.define('header-component', Header);
