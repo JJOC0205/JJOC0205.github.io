81a8b1413faea8523d56f1890f4a4002e9baaa2c
Jon O'Connell <jjoc0205@gmail.com>
2023-09-22 11:32:26 -0400

    got header export working

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ class Header extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
         <style>
-            .header{
+            nav{
                 background-color: rgb(250, 249, 250);
                 text-align: end;
                 overflow: auto;
@@ -24,13 +24,15 @@ class Header extends HTMLElement{
             }
         </style>
         <header>
-        <div class="header">
-            <a href="https://jfmarks.github.io/" class="links">Previous Student</a>
-            <a href="index.html" class="links">Home</a>
-            <a href="/aboutMe.html" class="links">About me page</a>
-            <a href="https://mattm401.github.io/" class="links">Course Site</a>
-            <a href="https://jthensley03.github.io/" class="links">Next Student</a>
-        </div>
+            <nav>
+                <div class="row">
+                    <a href="https://jfmarks.github.io/" class="links">Previous Student</a>
+                    <a href="index.html" class="links">Home</a>
+                    <a href="/aboutMe.html" class="links">About me page</a>
+                    <a href="https://mattm401.github.io/" class="links">Course Site</a>
+                    <a href="https://jthensley03.github.io/" class="links">Next Student</a>
+                </div>
+            </nav>
         </header>
     `;
     }
